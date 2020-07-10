1d23e62c4d289a021ee4035cc1d513a7e1f05f1f
Adam Wankowski <apiotrw1@gmail.com>
2020-07-10 23:45:39 +0200

    Update TerrainControls.cs

--- Assets/Scripts/TerrainControls.cs
+++ Assets/Scripts/TerrainControls.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class TerrainControls : MonoBehaviour
 {
+    Terrain terr; // terrain to modify
+    int hmWidth; // heightmap width
+    int hmHeight; // heightmap height
+
+    int posXInTerrain; // position of the game object in terrain width (x axis)
+    int posYInTerrain; // position of the game object in terrain height (z axis)
+
+    int size = 50; // the diameter of terrain portion that will raise under the game object
+    float desiredHeight = 0; // the height we want that portion of terrain to be
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
