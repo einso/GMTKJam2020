fc2f7b17dbbd00d59f0ff33986e242ef8772829f
Adam Wankowski <apiotrw1@gmail.com>
2020-07-12 00:56:43 +0200

    Update HolePlacement.cs

--- Assets/Scripts/HolePlacement.cs
+++ Assets/Scripts/HolePlacement.cs
@@ -6,16 +6,18 @@ public class HolePlacement : MonoBehaviour
 {
 
     public float heightToCastFrom = 100;
-    
+
+    Transform myTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTransform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 castPosition = new Vector3(myTransform.position.x, heightToCastFrom, myTransform.position.z);
     }
 }
