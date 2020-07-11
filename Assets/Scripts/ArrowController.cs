14f32b687ba19507ad8042f796587988b4b8ff89
Adam Wankowski <apiotrw1@gmail.com>
2020-07-11 14:31:33 +0200

    arrow

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -7,15 +7,18 @@ public class ArrowController : MonoBehaviour
     public Transform ball;
     public Transform cam;
 
+    Transform myTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTransform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(HelperFunctions.Test(Vector3.zero));
+        //print(HelperFunctions.Test(Vector3.zero));
+        myTransform.forward = cam.forward;
     }
 }
