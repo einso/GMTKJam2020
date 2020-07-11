c954b77e969dff9e71b45ca0632d805f7e84688b
Adam Wankowski <apiotrw1@gmail.com>
2020-07-11 17:35:39 +0200

    Update Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -30,6 +30,6 @@ public class Ball : MonoBehaviour
     void Update()
     {
         reloadTimer -= Time.deltaTime;
-        print(body.velocity + " " + Physics.OverlapSphere(body.position, 10).Length);
+        print(body.velocity.magnitude + " " + Physics.OverlapSphere(body.position, 10).Length);
     }
 }
