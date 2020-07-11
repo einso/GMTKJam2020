b2b75ee8588bf0a716231c05d1d3db15b88eeb15
Adam Wankowski <apiotrw1@gmail.com>
2020-07-11 14:39:12 +0200

    Update HelperFunctions.cs

--- Assets/Scripts/HelperFunctions.cs
+++ Assets/Scripts/HelperFunctions.cs
@@ -8,4 +8,15 @@ public class HelperFunctions
     {
         return Vector3.zero;
     }
+
+    public static Vector3 FlattenY(Vector3 a, bool normalized)
+    {
+
+        if (normalized)
+        {
+            return new Vector3(a.x, 0, a.z).normalized;
+        }
+
+        return new Vector3(a.x, 0, a.z);
+    }
 }
