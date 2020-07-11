2cae95702deecdb7464371fc81f85ebb0f00f773
Adam Wankowski <apiotrw1@gmail.com>
2020-07-11 15:21:18 +0200

    Update FPSDisplay.cs

--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -18,7 +18,7 @@ public class FPSDisplay : MonoBehaviour
 
         Rect rect = new Rect(0, 0, w, h * 2 / 100);
         style.alignment = TextAnchor.UpperLeft;
-        style.fontSize = h * 2 / 100;
+        style.fontSize = h * 2 / 50;
         style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
