2234579c473633da9166307d3191dffaf0709904
Adam Wankowski <apiotrw1@gmail.com>
2020-07-11 13:59:41 +0200

    Update CameraControls.cs

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
+    public Transform target;
+
+    Transform myTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTransform = transform;
     }
 
     // Update is called once per frame
