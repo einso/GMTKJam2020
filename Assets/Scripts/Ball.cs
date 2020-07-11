546750f52b1bd181b4e6f7528d2d1cbcc3e9ee9d
Adam Wankowski <apiotrw1@gmail.com>
2020-07-11 17:04:21 +0200

    Update Ball.cs

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public float power = 100;
+    public Transform arrow;
 
-    
+    Rigidbody body;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(arrow.forward * power);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
