b5bea33ba7bf5413708acd38625e0dff1749bd5b
Adam Wankowski <apiotrw1@gmail.com>
2020-07-11 14:03:12 +0200

    Update CameraControls.cs

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -7,15 +7,17 @@ public class CameraControls : MonoBehaviour
     public Transform target;
 
     Transform myTransform;
+    Vector3 lastPosition;
     // Start is called before the first frame update
     void Start()
     {
         myTransform = transform;
+        lastPosition = target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myTransform.position += target.position - lastPosition;
     }
 }
