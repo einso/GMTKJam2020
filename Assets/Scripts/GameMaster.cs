93f9da401c11fa64d7c3caeb60f5028144ed1aa5
Adam Wankowski <apiotrw1@gmail.com>
2020-07-12 15:39:21 +0200

    Update GameMaster.cs

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
+    [Header("Flag")]
     public float minFlagPosition, maxFlagPosition;
+    [Header("Terrain")]
     public float terrainSpeed, terrainFrequency, terrainAmplitude, terrainPersistance, terrainOctaves;
+    [Header("Ball")]
+    public float minBallPower, maxBallPower, minUpVector;
     // Start is called before the first frame update
     void Start()
     {
